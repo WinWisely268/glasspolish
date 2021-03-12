6b6bb6e78bdbc686ffc97c12db3d353e0b88a96f
Alexander Adhyatma <winwisely268@gmail.com>
2021-03-12 20:32:34 +0000

    added add to group lambda function for signup

--- backend/cmd/add-to-group/main.go
+++ backend/cmd/add-to-group/main.go
@@ -1,1 +1,20 @@
-package add_to_group
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/winwisely268/glasspolish-triggers/pkg/api"
+	"github.com/winwisely268/glasspolish-triggers/pkg/config"
+)
+
+var (
+	addToGroupApi *api.API
+)
+
+func init() {
+	newCfg := config.InitConfig()
+	addToGroupApi = api.NewAPI(newCfg)
+}
+
+func main() {
+	lambda.Start(addToGroupApi.AddUserToDefaultGroup)
+}
